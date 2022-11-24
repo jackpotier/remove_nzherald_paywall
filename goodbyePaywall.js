2731391df63ebc2a385799f7fa9b99f3a147c2be
Jack Potier <jackpotierschool@gmail.com>
2022-11-24 19:42:33 +1300

    changed text color

--- goodbyePaywall.js
+++ goodbyePaywall.js
@@ -2,6 +2,7 @@ function displayText(){
     const article = querySelectorAll(".zKYxPeaPPUabJsPV");
     article.forEach(zKYxPeaPPUabJsPV => {
         zKYxPeaPPUabJsPV.style.display = 'block';
+        zKYxPeaPPUabJsPV.style.color = '#000000';
     });
     console.log("hello");
 }
